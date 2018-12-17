1db3dbbae2e57e6b9d06bc7a6d338606500081c4
Alexander Joiseus <alexander.joiseus@gmail.com>
2018-12-17 08:51:57 -0500

    Create App, app builds successfully
    Initial creation of app through xcode. App complies and builds. Laucnhed ios simulator.

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Alexander Joiseus on 12/17/18.
+//  Copyright © 2018 Alexander Joiseus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
